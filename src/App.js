9cee7b00e9eff9c3357aee3f934b69a5f248f145
Wanessa <132595471+wanessa-antonio@users.noreply.github.com>
2026-02-26 15:30:25 -0300

    refactor: remove unused commented code from App component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-/*import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { HomePage } from './pages/Home/HomePage';
-import { MinhasPastasPage } from "./pages/MinhasPastas/MinhasPastasPage";
-import { HeaderPartial } from './partials/HeaderPartial/HeaderPartial';
-import { AppContext } from './store/AppContext';
-
-function App() {
-   return (
-  <BrowserRouter>
-    <div className="App">
-      <AppContext> 
-       <HeaderPartial />
-       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/minhas-pastas" element={<MinhasPastasPage/>}   />
-       </Routes>
-       </AppContext>
-    </div>
-  </BrowserRouter>
-  );
-}
-
-export default App;*/
-
 import {
   BrowserRouter,
   Routes,
